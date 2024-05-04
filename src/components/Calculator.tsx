3c39e11570d098bd99352b31586c3384949c5b70
Luis Meza <ednilsonluis.11@gmail.com>
2024-05-04 18:39:13 -0500

    Creacion del css para Calculator

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -4,16 +4,16 @@ const Calculator = () => {
   return (
     <section className='calculator'>
         <form className='form'>
-            <div>
-              <label htmlFor="inputBill">Factura Total: </label>
-              <input type="number" id="inputBill"/>
+            <div className='info bill'>
+              <label htmlFor="inputBill">Factura Total :</label>
+              <input type="number" id="inputBill" />
             </div>
-            <label htmlFor="porcentTip">Porcentaje de propina <input type="number" id="porcentTip"/> %</label>
-            <div>
-              <label htmlFor="montoTotal">Monto Total: </label>
+            <label htmlFor="porcentTip">Porcentaje de propina <input type="number" id="porcentTip" max={100}/> %</label>
+            <div className='info total'>
+              <label htmlFor="montoTotal">Monto Total :</label>
               <input type="text" readOnly id="montoTotal"/>
             </div>
-            <input type="submit" value={'Calcular'}/>
+            <input type="submit" value={'Calcular'} className='btnCalcular'/>
         </form>
     </section>
   )
